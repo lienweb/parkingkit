74cd83d975b2831300cb9dd3f73626ca4689ba85
lien <37633033+lienweb@users.noreply.github.com>
2022-11-09 21:57:22 +0800

    eslint init

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react/jsx-runtime',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // "quotes": ["error", "double"],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error'],
+  },
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+      <NavBar />
     </div>
   );
 }
