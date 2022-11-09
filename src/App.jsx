dc47999389b0b0b70669afce11e75f006cf6f740
lien <37633033+lienweb@users.noreply.github.com>
2022-11-09 22:30:57 +0800

    display map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/Navbar';
+import Map from './components/Map';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App row">
       <NavBar />
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,42 @@
+import 'leaflet/dist/leaflet.css';
+import './Map.scss';
+import L from 'leaflet';
+import {
+  MapContainer, TileLayer, ZoomControl, Marker, Popup,
+} from 'react-leaflet';
+import marker from '../assets/marker.png';
+
+function Map() {
+  const center = { lat: 25.026312001265776, lng: 121.5435894427204 };
+  const zoomLevel = 15;
+  const markerIcon = new L.Icon({
+    iconUrl: marker,
+    iconSize: [24, 36],
+    iconAnchor: [12, 36], // align when zoom in out
+    popupAnchor: [0, -46],
+  });
+
+  return (
+    <MapContainer
+      // style={{ height: '450px', width: '100%' }}
+      center={[center.lat, center.lng]}
+      zoom={zoomLevel}
+      scrollWheelZoom
+      zoomControl={false}
+      className="map__container"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www. openstreetmap.org/copyright">OpenStreetMap Contributors</ a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <ZoomControl position="bottomright" />
+      <Marker position={[center.lat, center.lng]} icon={markerIcon}>
+        <Popup>
+          <b>當前位置</b>
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default Map;
